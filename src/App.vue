d19572a0cb23dac099c70d2aa5478a69f8df66db
YegorMykhalechko <yegor.mykhalechko@gmail.com>
2022-04-05 12:56:52 +0200

    template

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <TimeLine />
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TimeLine from './components/TimeLine.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    TimeLine,
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'highlight.js/styles/atom-one-dark.css'
+
+const app = createApp(App)
+app.mount('#app')
