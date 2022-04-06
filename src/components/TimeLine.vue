1f710ff09c624b60d80f1d3a014c9f274638ee48
YegorMykhalechko <yegor.mykhalechko@gmail.com>
2022-04-06 21:42:17 +0200

    added testsuit for timeline component & suspence

--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -1,65 +1,79 @@
 <template>
   <nav class="is-primary panel">
     <span class="panel-tabs">
-      <a v-for="period of periods"
-         :key="period"
-         :class="{'is-active': period === currentPeriod}"
-         @click="setPeriod(period)"
+      <a
+        v-for="period of periods"
+        :key="period"
+        :class="{ 'is-active': period === currentPeriod }"
+        @click="setPeriod(period)"
+        :data-test="period"
       >
         {{ period }}
       </a>
     </span>
 
-    <a
-        v-for="post of posts"
-        :key="post.id"
-        class="panel-block">
+    <TimelinePost
+      v-for="post of posts"
+      :key="post.id"
+      :post="post"
+      class="panel-block"
+    >
       <a>{{ post.title }}</a>
-      <div>{{ post.created.format('Do MMM') }}</div>
-    </a>
+      <div>{{ post.created.format("Do MMM") }}</div>
+    </TimelinePost>
   </nav>
 </template>
 
 <script lang="ts">
-import {defineComponent, ref, computed} from 'vue';
+import { defineComponent, ref, computed } from "vue";
 import moment from "moment";
-import {today, thisWeek, thisMonth} from "@/mocks";
+import { today, thisWeek, thisMonth } from "@/mocks";
+import TimelinePost from "./TimelinePost.vue";
 
-type Period = 'Today' | 'This Week' | 'This Month'
+type Period = "Today" | "This Week" | "This Month";
+
+function delay() {
+  return new Promise(res => {
+    setTimeout(res, 2000);
+  });
+}
 
 export default defineComponent({
-  name: 'TimeLine',
+  name: "TimeLine",
+  components: {
+    TimelinePost
+  },
 
-  setup() {
-    const periods: Period[] = ['Today', 'This Week', 'This Month']
-    const currentPeriod = ref<Period>('Today')
+  async setup() {
+    await delay()
+    const periods: Period[] = ["Today", "This Week", "This Month"];
+    const currentPeriod = ref<Period>("Today");
     const posts = computed(() => {
       return [today, thisWeek, thisMonth].filter(post => {
-        if(currentPeriod.value === 'Today') {
-          return post.created.isAfter(moment().subtract(1, 'days'))
+        if (currentPeriod.value === "Today") {
+          return post.created.isAfter(moment().subtract(1, "days"));
         }
-        if(currentPeriod.value === 'This Week'){
-          return post.created.isAfter(moment().subtract(1, 'week'))
+        if (currentPeriod.value === "This Week") {
+          return post.created.isAfter(moment().subtract(1, "week"));
         }
-        if(currentPeriod.value === 'This Month'){
-          return post.created.isAfter(moment().subtract(1, 'month'))
+        if (currentPeriod.value === "This Month") {
+          return post.created.isAfter(moment().subtract(1, "month"));
         }
-      })
-    })
+      });
+    });
 
     const setPeriod = (period: Period) => {
-      currentPeriod.value = period
-    }
+      currentPeriod.value = period;
+    };
 
     return {
       posts,
       periods,
       currentPeriod,
       setPeriod
-    }
+    };
   }
 });
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
--- tests/unit/exemple.spec.ts
+++ tests/unit/exemple.spec.ts
@@ -1,11 +1,30 @@
+import { nextTick } from 'vue'
 import { mount } from '@vue/test-utils'
 import TimeLine from '../../src/components/TimeLine.vue'
+import { today, thisWeek, thisMonth } from '../../src/mocks'
 
 describe('TimeLine', () => {
     it('renders today post default', () => {
         const wrapper = mount(TimeLine)
+        expect(wrapper.html()).toContain(today.created.format('Do MMM'))
+    })
 
-        console.log(wrapper.html())
-        expect(wrapper.html()).toContain('6th Apr')
+    it('update when the period is click', async() => {
+        const wrapper = mount(TimeLine)
+
+        await wrapper.get('[data-test="This Week"]').trigger('click')
+
+        expect(wrapper.html()).toContain(today.created.format('Do MMM'))
+        expect(wrapper.html()).toContain(thisWeek.created.format('Do MMM'))
+    })
+
+    it('update when the period is click', async() => {
+        const wrapper = mount(TimeLine)
+
+        await wrapper.get('[data-test="This Month"]').trigger('click')
+
+        expect(wrapper.html()).toContain(today.created.format('Do MMM'))
+        expect(wrapper.html()).toContain(thisWeek.created.format('Do MMM'))
+        expect(wrapper.html()).toContain(thisMonth.created.format('Do MMM'))
     })
 })
