1cd7ea0c9f049a05bc08cb4c28e17a07abc1fcbe
YegorMykhalechko <yegor.mykhalechko@gmail.com>
2022-04-13 23:30:00 +0200

    added days

--- src/components/AvailabilityList.vue
+++ src/components/AvailabilityList.vue
@@ -1,6 +1,6 @@
 <template>
   <form @submit.prevent="addWaiter">
-    <input placeholder="Added Waiters" v-model="waiter.name"/>
+    <input placeholder="Added Waiters" v-model="waiter.name" />
     <button type="submit">Add</button>
   </form>
 
@@ -8,29 +8,34 @@
 
   <ul>
     <li v-for="(waiter, index) of waiters" :key="index">
-      {{waiter.name}}<button @click="showDetail(index)">{{ !waiter.detail ? 'Show more' : 'Show less' }}</button>
+      {{ waiter.name
+      }}<button @click="showDetail(index)">
+        {{ !waiter.detail ? "Show more" : "Show less" }}
+      </button>
       <div v-if="waiter.detail">
         <div>
           <span>Month:</span>
-          <strong>{{nextMonth}}</strong>
+          <strong>{{ nextMonth }}</strong>
         </div>
-        <Datepicker v-model="day"/>
+        <div>
+          <Datepicker v-model="day" />
+          <button @click="addDay">Add</button>
+        </div>
       </div>
     </li>
   </ul>
-  
 </template>
 
 <script lang="ts">
-import {computed, defineComponent, ref} from "vue";
+import { computed, defineComponent, ref } from "vue";
 import axios from "axios";
 import moment from "moment";
-import Datepicker from 'vue3-datepicker'
+import Datepicker from "vue3-datepicker";
 
 interface Waiter {
-  id: number
-  name: string
-  detail: boolean
+  id: number;
+  name: string;
+  detail: boolean;
 }
 
 export default defineComponent({
@@ -39,29 +44,39 @@ export default defineComponent({
     Datepicker
   },
   async setup() {
-
-    const res = await axios.get<Waiter[]>("http://localhost:3000/waiters")
+    const res = await axios.get<Waiter[]>("http://localhost:3000/waiters");
 
     const waiter = ref<Waiter>({
       id: Date.now(),
       name: "",
       detail: false
-    })
+    });
 
-    const waiters = ref<Waiter[]>(res.data)
+    const waiters = ref<Waiter[]>(res.data);
 
-    const nextMonth = ref<string>(moment().add(1, 'months').format('MMMM'))
+    const nextMonth = ref<string>(
+      moment()
+        .add(1, "months")
+        .format("MMMM")
+    );
 
-    const day = ref(new Date())
-    console.log(day.value)
-
+    const day = ref<Date>(new Date());
+    const days = ref<Date[]>([])
+  
     const addWaiter = () => {
-      axios.post<Waiter>(`http://localhost:3000/waiters`, waiter.value)
-      waiters.value.push(waiter.value)
-    }
+      const addedWaiter = waiter.value
+      axios.post<Waiter>(`http://localhost:3000/waiters`, addedWaiter)
+      waiters.value.push(addedWaiter)
+      waiter.value.name = ""
+    };
 
     const showDetail = (index: number) => {
-      waiters.value[index].detail = !waiters.value[index].detail
+      waiters.value[index].detail = !waiters.value[index].detail;
+    };
+
+    const addDay = () => {
+      days.value.push(day.value)
+      console.log(days.value)
     }
 
     return {
@@ -70,11 +85,11 @@ export default defineComponent({
       waiters,
       showDetail,
       nextMonth,
-      day
-    }
+      day,
+      addDay
+    };
   }
-})
+});
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
