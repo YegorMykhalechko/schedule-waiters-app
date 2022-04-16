72128015fc0c5137797e85d995955d7cc3b77389
YegorMykhalechko <yegor.mykhalechko@gmail.com>
2022-04-16 18:01:26 +0200

    deleted props in modal

--- src/useModal.ts
+++ src/useModal.ts
@@ -2,13 +2,11 @@ import { ref } from "vue";
 
 const show = ref(false)
 const component = ref()
-const props = ref()
 
 export function useModal() {
     return {
         component,
         show,
-        props,
         showModal: () => show.value = true,
         hideModal: () => show.value = false,
     }
